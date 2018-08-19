737e71baf3ca1b51000ba4ab3f159da162868749
nikieta <nikitamoghe29@gmail.com>
2018-08-19 14:40:57 +0530

    old ways

--- model.py
+++ model.py
@@ -496,11 +496,10 @@ class SummarizationModel(object):
             with tf.variable_scope('embedding'):
                 if hps.mode == "train":
                     if self.use_glove:
-  
                       tf.logging.info('glove')
-                      embedding = tf.get_variable('embedding', dtype=tf.float32, initializer=tf.cast(self._vocab.glove_emb,tf.float32),trainable=hps.emb_trainable)
+                      embedding = tf.get_variable('embedding', dtype=tf.float32, initializer=tf.cast(self._vocab.glove_emb,tf.float32))
                     else:
-                      embedding = tf.get_variable('embedding', [vsize, hps.emb_dim], dtype=tf.float32, initializer=self.trunc_norm_init,trainable=hps.emb_trainable)
+                      embedding = tf.get_variable('embedding', [vsize, hps.emb_dim], dtype=tf.float32, initializer=self.trunc_norm_init)
                 else:
                     embedding = tf.get_variable('embedding', [vsize, hps.emb_dim], dtype=tf.float32)
 
