10c3bb543a2c7c6d4e61a787013266740664abb0
cs16s016@cse.iitm.ac.in <cs16s016@cse.iitm.ac.in>
2018-09-11 14:21:05 +0530

    Bugs resolved

--- single_automate.py
+++ single_automate.py
@@ -6,29 +6,28 @@ import subprocess
 from multiprocessing import Pool
 
 file_name = sys.argv[1]
-gpu_id = sys.argv[2]
 #gpu_val_id = sys.argv[3]
 def run_train(file_name,out_file_name):
-	train_command = 'python run_summarization.py --mode=train --config_file ' + str(file_name) +' --gpu_device_id ' + str(gpu_id) + ' --use_gcn_before_lstm True'
+	train_command = 'python run_summarization.py --mode=train --config_file ' + str(file_name)
 	os.system(train_command)
 #	with open(out_file_name, "w") as outfile:
 #		subprocess.call(train_command, stdout=outfile)
 	
 def run_eval(file_name,out_file_name):
-	eval_command = 'python  run_summarization.py --mode=eval --config_file ' + str(file_name) +' --gpu_device_id ' + str(gpu_id) + ' --use_gcn_before_lstm True'
+	eval_command = 'python  run_summarization.py --mode=eval --config_file ' + str(file_name) 
 	eval_command = 'sleep 300; ' + eval_command
 	os.system(eval_command)
 	#with open(out_file_name, "w") as outfile:
 	#	subprocess.call(eval_command, stdout=outfile)
 
 def run_test(file_name,out_file_name):
-	test_command = 'python -u run_summarization.py --mode=decode --config_file '+str(file_name)+ ' --gpu_device_id ' + str(gpu_id) + ' --use_gcn_before_lstm True' 
+	test_command = 'python -u run_summarization.py --mode=decode --config_file '+str(file_name) 
 	#with open(out_file_name, "w") as outfile:
 	#	subprocess.call(test_command, stdout=outfile)
 	os.system(test_command)
 
 def run_eval_test(file_name,out_file_name):
-	test_command = 'python -u run_summarization.py --mode=decode --use_val_as_test=True --config_file ' + str(file_name) +' --gpu_device_id ' + str(gpu_id) + ' --use_gcn_before_lstm True'
+	test_command = 'python -u run_summarization.py --mode=decode --use_val_as_test=True --config_file ' + str(file_name)
 	#with open(out_file_name, "w") as outfile:
 	#	subprocess.call(test_command, stdout=outfile)
 	os.system(test_command)	
