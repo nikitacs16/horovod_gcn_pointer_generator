ff73f7944a7a9cf6f384fde1f8b5cf5bd52bf065
Nikita <cs16s016@cse.iitm.ac.in>
2019-02-19 13:46:16 +0530

    docker hack

--- util.py
+++ util.py
@@ -24,6 +24,9 @@ FLAGS = tf.app.flags.FLAGS
 def get_config():
   """Returns config for tf.session"""
   config = tf.ConfigProto(allow_soft_placement=True)
+  config.inter_op_parallelism_threads = 44
+  config.intra_op_parallelism_threads = 44
+
   config.gpu_options.allow_growth=True
   return config
 
